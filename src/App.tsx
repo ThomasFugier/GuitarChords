710971e397413e85f08e36b6019cb0d0457db08f
Thomas Fugier <74909360+ThomasFugierVoodoo@users.noreply.github.com>
2025-07-01 18:57:58 +0200

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GuitarChordVisualizer } from "./components/GuitarChordVisualizer";
+import { GuitarNeck } from "./components/GuitarNeck";
 
 const App: React.FC = () => (
   <main className="min-h-screen flex items-start justify-center">
